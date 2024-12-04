92862acd3aca819cba984752f68897790c7bc33b
Prit Dhanani <49144354+preetdhanani@users.noreply.github.com>
2024-12-04 23:51:26 +0100

    changes

--- AskToYaana/AskToYaana/settings.py
+++ AskToYaana/AskToYaana/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,12 +78,20 @@ WSGI_APPLICATION = "AskToYaana.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#         'ATOMIC_REQUESTS': True,
+#     }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        'ATOMIC_REQUESTS': True,
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://asktoyaana_db_user:lNr4kgsRMpNdojfyOXC4A5RwAdmDuniG@dpg-ct8ch6hu0jms73e9v1v0-a/asktoyaana_db',
+        conn_max_age=600
+    )
+}
 
     # "default": {
     #     "ENGINE": "django.db.backends.mysql",
